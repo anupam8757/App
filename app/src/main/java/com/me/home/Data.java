c5c68b4882cc1be61c87fac9259d99d4e1afaa0b
Sujeet Suman <sujeetxavier793@gmail.com>
2020-08-27 13:25:04 +0530

    image is added in main activity

--- app/src/main/java/com/me/home/Data.java
+++ app/src/main/java/com/me/home/Data.java
@@ -12,18 +12,18 @@ public class  Data {
     public static   ArrayList<Main_list_item> assign_main_item() {
 
         ArrayList<Main_list_item> main_list_items=new ArrayList<Main_list_item>();
-        main_list_items.add(new Main_list_item(R.drawable.food_min_min,"RESTAURANT FOODS & CUISINES"));
+        main_list_items.add(new Main_list_item(R.drawable.restrorennnttt,"RESTAURANT FOODS & CUISINES"));
         main_list_items.add(new Main_list_item(R.drawable.bakerycakesanddairy_min,"BAKERY,CAKES & DAIRY"));
         main_list_items.add(new Main_list_item(R.drawable.vegetable,"VEGETABLE"));
         main_list_items.add(new Main_list_item(R.drawable.grocery_min,"GROCERIES"));
         main_list_items.add(new Main_list_item(R.drawable.fruits,"FRUITS"));
         main_list_items.add(new Main_list_item(R.drawable.beverages_min,"BEVERAGES"));
         main_list_items.add(new Main_list_item(R.drawable.snacks_min,"SNACKS & PACKED FOOD"));
-        main_list_items.add(new Main_list_item(R.drawable.patanjali_min,"PATANJALI ITEMS"));
-        main_list_items.add(new Main_list_item(R.drawable.household_min,"CLEANING & HOUSEHOLD"));
+        main_list_items.add(new Main_list_item(R.drawable.patanjali,"PATANJALI ITEMS"));
+        main_list_items.add(new Main_list_item(R.drawable.cleanin,"CLEANING & HOUSEHOLD"));
         main_list_items.add(new Main_list_item(R.drawable.sweets,"SWEETS"));
         main_list_items.add(new Main_list_item(R.drawable.nonveg_min,"EGG,MEAT & FISH"));
-        main_list_items.add(new Main_list_item(R.drawable.babycare_min,"BABY CARE"));
+        main_list_items.add(new Main_list_item(R.drawable.babycare,"BABY CARE"));
         main_list_items.add(new Main_list_item(R.drawable.other_min,"OTHER ITEMS"));
 
 
