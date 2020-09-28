a3302e5ba81f5c847c284072f442fd0500b2d443
Sujeet Suman <sujeetxavier793@gmail.com>
2020-09-28 15:28:43 +0530

    the amul catogaries is added

--- app/src/main/java/com/yukkti/home/Data.java
+++ app/src/main/java/com/yukkti/home/Data.java
@@ -13,17 +13,17 @@ public class  Data {
         main_list_items.add(new Main_list_item(R.mipmap.restrorennnttt,"RESTAURANT FOODS & CUISINES"));
         main_list_items.add(new Main_list_item(R.mipmap.bakerycakesanddairy_min,"BAKERY,CAKES & DAIRY"));
         main_list_items.add(new Main_list_item(R.mipmap.sweets,"SWEETS"));
+        main_list_items.add(new Main_list_item(R.mipmap.amul,"AMUL"));
+        main_list_items.add(new Main_list_item(R.mipmap.ptanajli,"PATANJALI ITEMS"));
         main_list_items.add(new Main_list_item(R.mipmap.grocery_min,"GROCERIES"));
         main_list_items.add(new Main_list_item(R.mipmap.fruits,"FRUITS"));
         main_list_items.add(new Main_list_item(R.mipmap.beverages_min,"BEVERAGES"));
         main_list_items.add(new Main_list_item(R.mipmap.snacks_min,"SNACKS & PACKED FOOD"));
-        main_list_items.add(new Main_list_item(R.mipmap.ptanajli,"PATANJALI ITEMS"));
         main_list_items.add(new Main_list_item(R.mipmap.cleanin,"CLEANING & HOUSEHOLD"));
         main_list_items.add(new Main_list_item(R.mipmap.nonveg_min,"EGG,MEAT & FISH"));
         main_list_items.add(new Main_list_item(R.mipmap.babycare,"BABY CARE"));
         main_list_items.add(new Main_list_item(R.mipmap.new_vegetable,"VEGETABLE"));
         main_list_items.add(new Main_list_item(R.mipmap.other_min,"OTHER ITEMS"));
-
         return main_list_items;
     }
 }
